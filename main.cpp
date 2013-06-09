1e57d469bda9ba4ea9946ab4d19daee7d932b786
范翔 <fanxiang@FanXiang-Mac.local>
2013-06-09 11:19:16 +0800

    服务器往客户端发送消息

--- main.cpp
+++ main.cpp
@@ -16,51 +16,52 @@
 
 int main(int argc, char *argv[])
 {
-    //QCoreApplication a(argc, argv);
-    // hello
+    QCoreApplication a(argc, argv);
     int m_dServer;
-        sockaddr_in m_Addr;
-        m_dServer = socket(AF_INET, SOCK_STREAM, 0);
-        if(m_dServer<0)
+    sockaddr_in m_Addr;
+    m_dServer = socket(AF_INET, SOCK_STREAM, 0);
+    if(m_dServer<0)
+    {
+        printf("Error!");
+        return 0;
+    }
+    memset(&m_Addr, 0, sizeof(m_Addr));
+    m_Addr.sin_family = AF_INET;
+    m_Addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    m_Addr.sin_port = htons(5500);
+    bind(m_dServer,(struct sockaddr*)&m_Addr,sizeof(m_Addr));
+    if(listen(m_dServer,3)<0)
+    {
+        return 0;
+    }
+    int clientid;
+    sockaddr sad;
+    socklen_t sadlen = sizeof(sad);
+    while(true)
+    {
+        clientid = accept(m_dServer,&sad,&sadlen);
+        pid_t tid = fork();
+        if(tid == 0)
         {
-            printf("sdfsdfsd");
-            return 0;
-        }
-        memset(&m_Addr, 0, sizeof(m_Addr));
-        m_Addr.sin_family = AF_INET;
-        m_Addr.sin_addr.s_addr = htonl(INADDR_ANY);
-        m_Addr.sin_port = htons(5500);
-        bind(m_dServer,(struct sockaddr*)&m_Addr,sizeof(m_Addr));
-        if(listen(m_dServer,3)<0)
-        {
-            return 0;
-        }
-        int clientid;
-        sockaddr sad;
-        socklen_t sadlen = sizeof(sad);
-        while(true)
-        {
-            clientid = accept(m_dServer,&sad,&sadlen);
-            pid_t tid = fork();
-            if(tid == 0)
+            pid_t mid = getpid();
+            close(m_dServer);
+            char buf[512];
+            int len;
+            while(true)
             {
-                pid_t mid = getpid();
-                close(m_dServer);
-                char buf[512];
-                    int len;
-                while(true)
-                {
-                        len = read(clientid,buf,512);
-                        buf[len] = 0;
-                        printf("pid = %d data=%s\n",mid,buf);
-
-                }
-            }
-            else
-            {
-                close(clientid);
+                    len = read(clientid,buf,512);
+                    buf[len] = 0;
+                    printf("pid = %d data=%s\n",mid,buf);
+                    //往客服端发送消息
+                    write(clientid,buf,len);
             }
         }
-    //return a.exec();
+        else
+        {
+            printf("%d",tid);
+            close(clientid);
+        }
+    }
+    return a.exec();
     return 0;
 }
